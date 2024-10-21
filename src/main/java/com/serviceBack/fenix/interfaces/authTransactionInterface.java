f4398f860256c7a1d5b9ad814b1ceeba4fd3b9df
Augusto Gomez <agr120@hotmail.com>
2024-10-20 19:51:42 -0600

    AGOMEZ_20241020 RENAME EN INTERFACE

--- src/main/java/com/serviceBack/fenix/interfaces/authTransactionInterface.java
+++ src/main/java/com/serviceBack/fenix/interfaces/authTransactionInterface.java
@@ -3,7 +3,7 @@ package com.serviceBack.fenix.interfaces;
 import com.serviceBack.fenix.Utils.ResponseService;
 import com.serviceBack.fenix.models.AuthTransaction;
 
-public interface authTransactionInterface {
+public interface AuthTransactionInterface {
 
     public ResponseService authTransaction(AuthTransaction authTransaction);
     
